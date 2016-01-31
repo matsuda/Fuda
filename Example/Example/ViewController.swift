3bdc14d0d8edbccaac1b473fb945dc61b02386d1
matsuda <kosukematsuda@gmail.com>
2016-02-01 00:28:05 +0900

    add sample code

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import Fuda
 
 class ViewController: UIViewController {
+
+    @IBOutlet weak var badgeButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        let item = UIBarButtonItem(title: "item", badge: "9", target: self, action: "tapBarItem:")
+        self.navigationItem.rightBarButtonItem = item
+
+        let button = UIButton(type: .Custom, badge: "8")
+        button.setTitle("button", forState: .Normal)
+        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        button.sizeToFit()
+        button.frame = {
+            var frame = button.frame
+            frame.origin = CGPoint(
+                x: (self.view.frame.size.width - frame.size.width) / 2,
+                y: 200)
+            return frame
+        }()
+        self.view.addSubview(button)
+
+        badgeButton.badgeText = "99"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +41,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tapBarItem(sender: AnyObject) {
+        print(__FUNCTION__)
+    }
 }
 
