6b75f8179eb06e953f88b264776340f0ae1f152f
matsuda <kosukematsuda@gmail.com>
2016-01-31 22:15:36 +0900

    create project

---
+++ Fuda/Fuda.h
@@ -0,0 +1,19 @@
+//
+//  Fuda.h
+//  Fuda
+//
+//  Created by Kosuke Matsuda on 2016/01/31.
+//  Copyright © 2016年 Kosuke Matsuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Fuda.
+FOUNDATION_EXPORT double FudaVersionNumber;
+
+//! Project version string for Fuda.
+FOUNDATION_EXPORT const unsigned char FudaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Fuda/PublicHeader.h>
+
+
